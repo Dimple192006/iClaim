13c0e13f9d47960a72794a8a2122b0236b4ea77e
Anushka Varshney <vikashvarshney33@gmail.com>
2025-10-04 19:15:10 +0530

    saved

---
+++ frontend/src/App.tsx
@@ -0,0 +1,32 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import FileClaim from "./pages/FileClaim";
+import CustomerDashboard from "./pages/CustomerDashboard";
+import AgentDashboard from "./pages/AgentDashboard";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/file-claim" element={<FileClaim />} />
+          <Route path="/customer" element={<CustomerDashboard />} />
+          <Route path="/agent" element={<AgentDashboard />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
